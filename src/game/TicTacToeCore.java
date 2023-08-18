6643aaeed48089053b59073531e3c21fa3eac6fe
taktakgyun <vbmrkk@naver.com>
2023-08-18 11:22:39 +0900

    restart

--- src/game/TicTacToeCore.java
+++ src/game/TicTacToeCore.java
@@ -13,15 +13,15 @@ public class TicTacToeCore {
 	private int currentTurn = 1;
 		
 	public TicTacToeCore(int currentPlayerNum) {
+		this.resetGame(currentPlayerNum);
+	}
+
+	public void resetGame(int currentPlayerNum) {
 		this.resetTurn();
 		this.setCurrentPlayerNum(currentPlayerNum);
 	}
-
-	public void changeTurn() {
-		currentPlayerNum = (currentPlayerNum == 1) ? 2 : 1;
-		this.countTurn();
-	}
-
+	
+	// About Player Order
 	public int getCurrentPlayerNum() {
 		return currentPlayerNum;
 	}
@@ -30,6 +30,11 @@ public class TicTacToeCore {
 		this.currentPlayerNum = currentPlayerNum;
 	}	
 	
+	// About Turn
+	public void changeTurn() {
+		currentPlayerNum = (currentPlayerNum == 1) ? 2 : 1;
+		this.countTurn();
+	}
 	public void countTurn() {
 		this.currentTurn++;
 	}
@@ -39,56 +44,22 @@ public class TicTacToeCore {
 	public boolean isFullMap() {
 		return (this.currentTurn==9);
 	}
-
+	
 	/**
 	 * 
+	 * 현제 맵상태를 넣으면, 승패를 판정해주는 함수
 	 * @param currentStage
-	 * @return GAME_OVER(-99): 게임종료됨, WIN_P1(1): 플레이어 1 승리, WIN_P2(2): 플레이어 2 승리, PLAY(0): 진행중, DRAW(99): 비김(draw)
+	 * @return WIN_P1(1): 플레이어 1 승리, WIN_P2(2): 플레이어 2 승리, PLAY(0): 진행중, DRAW(99): 비김(draw)
 	 */
 	public STATE inputCurrentStage(int[][] currentStage) {
+		// 승패 판정하기
+		// 코드를 작성하시오
 		
-		for(int i = 0; i < currentStage.length; i++) {
-			String rowStr = "";
-			String colStr = "";
-			String diagStr1 = "";
-			String diagStr2 = "";
-			for(int j = 0; j < currentStage[i].length; j++) {
-				rowStr += (currentStage[i][j] + "");
-				colStr += (currentStage[j][i] + "");
-			}
-			for(int j = 0; j < currentStage.length; j++) {
-				diagStr1 += currentStage[j][j];
-				diagStr2 += currentStage[j][2 - j];
-			}			
-			
-			// 가로 판단			
-			
-			if(isThisPlayerWin(2, rowStr, colStr, diagStr1, diagStr2)) {
-				return STATE.WIN_P2;
-			} else if(isThisPlayerWin(1, rowStr, colStr, diagStr1, diagStr2)) {
-				return STATE.WIN_P1;
-			} else if(this.isFullMap()) {
-				return STATE.DRAW;
-			} else {
-				continue;
-			}
+		// 비기는 경우
+		if(this.isFullMap()) { //맵이 꽉찼는데도 승패 판정을 못했다.
+			return STATE.DRAW;
 		}
+		
 		return STATE.PLAY;
-	}
-	
-	private static boolean isThisPlayerWin(int playerNum, String rowFrag, String colFrag, String diagFrag1, String diagFrag2) {
-		String p = String.valueOf(playerNum);
-		boolean result = false;
-		String[] arr = {rowFrag, colFrag, diagFrag1, diagFrag2};
-		for(int i = 0; i < arr.length; i++) {
-			result = !arr[i].contains("0") && arr[i].equals(p + p + p);
-			if (result)	return result;
-		}
-		return result;
-	}
-	
-	public void resetGame(int currentPlayerNum) {
-		this.currentPlayerNum = currentPlayerNum;
-		this.resetTurn();
 	}
 }
